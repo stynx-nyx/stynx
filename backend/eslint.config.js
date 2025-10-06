4effe1f1181cc0d24daf7f17e8fdc63adb7336af
Antonio A. Russo <aarusso@nyxk.com.br>
2025-10-05 21:02:54 -0300

    chore(lint): migrate backend eslint config to flat config

---
+++ backend/eslint.config.js
@@ -0,0 +1,21 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+);
